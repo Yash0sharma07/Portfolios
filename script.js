8d9250e65fa123b0ac36c50d2501ed3425454cfc
Yash Sharma <165887207+Yash0sharma07@users.noreply.github.com>
2025-03-26 02:55:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-<script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script>
-    <script>
-
-
 var typed = new Typed("#element", {
         strings: [
-          "Web Developer .",
-          "Frontend Developer .",
+          "Web Developer.",
+          "Frontend Developer.",
           "UI UX Designer.",
           "Web Designer.",
         ],
